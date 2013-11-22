b74acb9bd00b876e6557d197bce6334f2b53f606
epigramedia <addaraquthny@yahoo.co.id>
2013-11-22 07:03:01 +0700

    change module name ;-)

--- tastypie.js
+++ tastypie.js
@@ -1,4 +1,4 @@
-angular.module('ng.tastypie', [])
+angular.module('tyaslab.tastypie', [])
 .factory('tastypieService', function($http, $q) {
     var _tastypieService = function(url, fk, m2m) {
         var thisClass = this; // just the reference, thisClass self explanatory ;-)
